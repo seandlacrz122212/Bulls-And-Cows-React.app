f6555792a4b03ad03c3beb9dab174d5dae2281e1
Sean Dela Cruz <andreadlacrz1@gmail.com>
2021-09-21 15:54:36 +0800

    Bulls & Cows

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import {
+  useState,
+  useEffect 
+} 
+from "react";
+
+//this is to make a random number that is hidden or unknown//
+const randomNumber = () => {
+  const generateRandomNumber = Math.floor(Math.random() * 9000 + 1000);
+  if (hiddenRandomNumber(generateRandomNumber)) {
+    return generateRandomNumber;
+  } else {
+    return randomNumber;
+  }
+};
+
+//this will check if there's a duplicate number or digit in the numbercombination//
+const hiddenRandomNumber = (fourDigits) => {
+  return !/(.).*?\1/.test(fourDigits);
+};
+
+const testInput = (randomNumber, inputData) => {
+  let bull = 0;
+  let cow = 0; //loops
+  for (let i = 0; i < randomNumber.length; i++) { //
+    if (
+      inputData.includes(randomNumber[i]) &&
+      randomNumber[i] === inputData[i]
+    ) {
+      bull++;
+//if player got the correct answer//
+      if (bull === 4) {
+        return alert("4 STRAIGHT BULLS!");
+      }
+    } else if (inputData.includes(randomNumber[i])) {
+      cow++;
+    }
+  }
+  return {
+    inputData: inputData,
+    bull: bull,
+    cow: cow,
+  };
+};
+
 
 function App() {
+  useEffect(() => {
+    setgenerated(randomNumber());
+  }, []);
+
+  const [generatedNumber, setgenerated] = useState(0);
+  const [checkBull, setcheckBull] = useState(0);
+  const [checkCow, setcheckCow] = useState(0);
+  const [inputData, setData] = useState("");
+  const [historyInput, sethistoryInput] = useState([]);
+  
+//alerts//
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputData === "") {
+      return alert("You have to guess a number.");
+    } else if (inputData.length < 4) {
+      return alert("You're missing a number.");
+    }
+
+  
+    const testresult = testInput(generatedNumber.toString(), inputData);
+    setcheckBull(testresult.bull);
+    setcheckCow(testresult.cow);
+
+    if (historyInput.length === 0) {
+      sethistoryInput([
+        testresult,
+      ]);
+    } else {
+      sethistoryInput((history) => [
+        ...history,
+        testresult,
+      ]);
+    }
+  };
+
+  
+  const handleInput = (e) => {
+    console.log(e.target.value);
+    setData(e.target.value);
+  };
+
+  //JSX
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className='main-game'>
+        <div className='main-header'>
+          <h1>Bulls & Cows Game!</h1>
+          <p></p>
+        </div>
+        <div className='main-input'>
+          <form action=''>
+            <input type='text' required maxLength='4' onChange={handleInput} />
+            <button onClick={handleSubmit}>Guess!</button>
+          </form>
+        </div>
+        <div className='main-display'>
+        <div className='display-input'>
+            <ul>
+              {historyInput.map((history, index) => {
+                return (
+                  <li key={index}>
+                    {history.inputData}
+                    {/* {history.bull} {history.cow} */}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className='display-result'>
+            <h3>
+              Bulls: {checkBull} Cows: {checkCow}
+            </h3>
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+export { randomNumber, hiddenRandomNumber, testInput };
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App,{randomNumber,hiddenRandomNumber, testInput} from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
+
+describe('Function for Generating Random Number', () => {
+  it('Function for Generating Random Number', () => {
+    expect(randomNumber()).not.toBe(null)
+  })
+  it('Number should be unique', () => {
+    expect(hiddenRandomNumber(randomNumber())).toBe(true)
+    expect(hiddenRandomNumber(1234)).toBe(false)
+  })
+})
+
+describe('inputData will be Unique', () => {
+  const result = testInput('1234', '1234')
+  it('Needs to chekc if there is input', () => {
+    expect (typeof result).toBe('object')
+    expect (result.inputData && result.cow >= 0 && result.bull >=0).toBe(true)
+  })
+})
+
